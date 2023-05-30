af5113c0106a007f52f0c244eddad15daa60c723
DESKTOP-VGH69S8\AlexeyShakov <alexei_96@inbox.ru>
2023-05-30 22:50:31 +0300

    refactored login

--- src/registration/utils.py
+++ src/registration/utils.py
@@ -1,8 +1,10 @@
 from fastapi import Depends
+from fastapi_jwt_auth import AuthJWT
 
 from database_conn import Base, get_async_session
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select
+from config import JWT_ALGORITHM
 
 
 async def get_obj(model: Base, session: AsyncSession, user_id: int = None, username: str = None) -> Base | bool:
@@ -15,3 +17,9 @@ async def get_obj(model: Base, session: AsyncSession, user_id: int = None, usern
     if not obj:
         return False
     return obj
+
+
+async def create_tokens(username: str, additional_info: dict, token_creator: AuthJWT) -> dict:
+    access_token = token_creator.create_access_token(subject=username, user_claims=additional_info, algorithm=JWT_ALGORITHM)
+    refresh_token = token_creator.create_refresh_token(subject=username, user_claims=additional_info, algorithm=JWT_ALGORITHM)
+    return {"access_token": access_token, "refresh_token": refresh_token}
